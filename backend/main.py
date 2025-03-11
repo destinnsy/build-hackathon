1272ba54ed5d01c6ae3523b017271c63dd7f74b2
Destin Ngeow <destin_ngeow@tech.gov.sg>
2025-03-11 20:01:45 +0800

    Modify CORS middleware to allow all origins

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "*.airbase.sg", "*.up.railway.app", "*.railway.internal"],  # Your frontend URL
+    allow_origins=["*"],  # Your frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
